777568d06353f4f39ba07c614d1ac098f2ca7b33
james137137 <james137137@gmail.com>
2017-03-02 17:31:21 +1300

    Minor error fix

--- src/main/java/nz/co/lolnet/mercury/api/lolcon/GetForumUserForumGroups.java
+++ src/main/java/nz/co/lolnet/mercury/api/lolcon/GetForumUserForumGroups.java
@@ -28,84 +28,82 @@ import nz.co.lolnet.mercury.util.JsonResponse;
  *
  * @author James
  */
+@Path("/lolcon/getforumuserforumgroups")
 public class GetForumUserForumGroups {
 
-    @Path("/lolcon/getforumuserforumgroups")
-    public class GetPlayerBalance {
+    private final String permission = "LolCon.getforumuserforumgroups";
 
-        private final String permission = "LolCon.getforumuserforumgroups";
-
-        @GET
-        @Produces(MediaType.APPLICATION_JSON)
-        public Response doGet() {
-            return Response.status(Response.Status.BAD_REQUEST).entity(JsonResponse.error("Bad Request", "Bad request")).build();
-        }
-
-        @GET
-        @Path("{request}/")
-        @Produces(MediaType.APPLICATION_JSON)
-        public Response doGet(@PathParam("request") String request) {
-
-            String forumGroups = "";
-            Authentication authentication = new Authentication();
-            Response response = authentication.checkAuthentication(request, permission);
-
-            if (response.getStatus() != Response.Status.ACCEPTED.getStatusCode()) {
-                return response;
-            }
-
-            Data data = authentication.getData();
-            if (!response.hasEntity() || !(response.getEntity() instanceof JsonObject)) {
-                data.setMessage(authentication.doEncrypt(JsonResponse.error("Internal Server Error", "Invalid response")));
-                return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(new Gson().toJson(data)).build();
-            }
-
-            JsonObject jsonObject = (JsonObject) response.getEntity();
-            if (!jsonObject.has("userforumid")) {
-                data.setMessage(authentication.doEncrypt(JsonResponse.error("Bad Request", "Request is missing 'userforumid'")));
-                return Response.status(Response.Status.BAD_REQUEST).entity(new Gson().toJson(data)).build();
-            }
-
-            int userForumID = jsonObject.get("userForumID").getAsInt();
-
-            MySQL mysql = new MySQL(Mercury.getInstance().getConfig().getDatabases().get("forum"));
-            Connection connection = null;
-            PreparedStatement preparedStatement = null;
-            ResultSet resultSet = null;
-            try {
-                connection = mysql.getMySQLConnection();
-                if (connection == null) {
-                    throw new SQLException("Connection is null!");
-                }
-
-                preparedStatement = connection.prepareStatement("SELECT user_group_id FROM xenforo.xf_user_group_relation WHERE `user_id`=?");
-                preparedStatement.setInt(1, userForumID);
-                resultSet = preparedStatement.executeQuery();
-                while (resultSet.next()) {
-
-                    int groupID = resultSet.getInt("user_group_id");
-                    if (groupID > 0) {
-                        forumGroups += groupID + "~";
-                    }
-                }
-
-                jsonObject = new JsonObject();
-                jsonObject.addProperty("userForumID", userForumID);
-
-                jsonObject.addProperty("forumGroups", forumGroups);
-
-                data.setMessage(authentication.doEncrypt(new Gson().toJson(jsonObject)));
-                jsonObject = null;
-
-                return Response.status(Response.Status.OK).entity(new Gson().toJson(data)).build();
-            } catch (SQLException ex) {
-                ConsoleOutput.error("Encountered an error processing 'getforumuserforumgroups " + userForumID + "' - SQLException");
-                ex.printStackTrace();
-            } finally {
-                mysql.closeMySQL(connection, preparedStatement, resultSet);
-                mysql = null;
-            }
-            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(JsonResponse.error("InternalServerError", "Unable to process request.")).build();
-        }
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response doGet() {
+        return Response.status(Response.Status.BAD_REQUEST).entity(JsonResponse.error("Bad Request", "Bad request")).build();
     }
+
+    @GET
+    @Path("{request}/")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response doGet(@PathParam("request") String request) {
+
+        String forumGroups = "";
+        Authentication authentication = new Authentication();
+        Response response = authentication.checkAuthentication(request, permission);
+
+        if (response.getStatus() != Response.Status.ACCEPTED.getStatusCode()) {
+            return response;
+        }
+
+        Data data = authentication.getData();
+        if (!response.hasEntity() || !(response.getEntity() instanceof JsonObject)) {
+            data.setMessage(authentication.doEncrypt(JsonResponse.error("Internal Server Error", "Invalid response")));
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(new Gson().toJson(data)).build();
+        }
+
+        JsonObject jsonObject = (JsonObject) response.getEntity();
+        if (!jsonObject.has("userforumid")) {
+            data.setMessage(authentication.doEncrypt(JsonResponse.error("Bad Request", "Request is missing 'userforumid'")));
+            return Response.status(Response.Status.BAD_REQUEST).entity(new Gson().toJson(data)).build();
+        }
+
+        int userForumID = jsonObject.get("userForumID").getAsInt();
+
+        MySQL mysql = new MySQL(Mercury.getInstance().getConfig().getDatabases().get("forum"));
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        try {
+            connection = mysql.getMySQLConnection();
+            if (connection == null) {
+                throw new SQLException("Connection is null!");
+            }
+
+            preparedStatement = connection.prepareStatement("SELECT user_group_id FROM xenforo.xf_user_group_relation WHERE `user_id`=?");
+            preparedStatement.setInt(1, userForumID);
+            resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+
+                int groupID = resultSet.getInt("user_group_id");
+                if (groupID > 0) {
+                    forumGroups += groupID + "~";
+                }
+            }
+
+            jsonObject = new JsonObject();
+            jsonObject.addProperty("userForumID", userForumID);
+
+            jsonObject.addProperty("forumGroups", forumGroups);
+
+            data.setMessage(authentication.doEncrypt(new Gson().toJson(jsonObject)));
+            jsonObject = null;
+
+            return Response.status(Response.Status.OK).entity(new Gson().toJson(data)).build();
+        } catch (SQLException ex) {
+            ConsoleOutput.error("Encountered an error processing 'getforumuserforumgroups " + userForumID + "' - SQLException");
+            ex.printStackTrace();
+        } finally {
+            mysql.closeMySQL(connection, preparedStatement, resultSet);
+            mysql = null;
+        }
+        return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(JsonResponse.error("InternalServerError", "Unable to process request.")).build();
+    }
+
 }
